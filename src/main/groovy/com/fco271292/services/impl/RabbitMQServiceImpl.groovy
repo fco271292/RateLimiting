efa69ead3ce5cc448743c9d979ae832b6b4cf339
Francisco Reyes <juan271292@hotmail.com>
2022-09-08 09:25:04 -0500

    Se agrega rabbitmq

--- src/main/groovy/com/fco271292/services/impl/RabbitMQServiceImpl.groovy
+++ src/main/groovy/com/fco271292/services/impl/RabbitMQServiceImpl.groovy
@@ -2,6 +2,7 @@ package com.fco271292.services.impl
 
 import java.time.LocalDateTime
 
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -10,8 +11,12 @@ import com.fco271292.rabbitmq.RabbitMQConfig
 import com.fco271292.rabbitmq.Receiver
 import com.fco271292.services.RabbitMQService
 
+import ch.qos.logback.classic.Logger
+
 @Service
 class RabbitMQServiceImpl implements RabbitMQService {
+	
+	Logger logger = LoggerFactory.getLogger(this.class)
 
 	@Autowired
 	RabbitTemplate rabbitTemplate
@@ -20,8 +25,14 @@ class RabbitMQServiceImpl implements RabbitMQService {
 	
 	@Override
 	def run() {
-		def message = "RABBITMQ ${LocalDateTime.now()}" as String
-		rabbitTemplate.convertAndSend(RabbitMQConfig.topicExchangeName, RabbitMQConfig.routingKey, message)
+		
+		try {
+			def message = "RABBITMQ ${LocalDateTime.now()}" as String
+			rabbitTemplate.convertAndSend(RabbitMQConfig.topicExchangeName, RabbitMQConfig.routingKey, message)
+		} catch (all) {
+			logger.error "${all.message}"
+		}
+		
 	}
 	
 }
